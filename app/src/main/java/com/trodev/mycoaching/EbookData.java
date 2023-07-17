02d982e7db1bd07f2934f55a18ee347f16e46c73
zobayerdev <zobayer.dev@gmail.com>
2023-07-17 23:25:25 +0600

    exam syllabus all work complete

---
+++ app/src/main/java/com/trodev/mycoaching/EbookData.java
@@ -0,0 +1,25 @@
+package com.trodev.mycoaching;
+
+public class EbookData {
+
+    private String pdfTitle,pdfUrl;
+
+    public EbookData() {
+    }
+
+    public String getPdfTitle() {
+        return pdfTitle;
+    }
+
+    public void setPdfTitle(String pdfTitle) {
+        this.pdfTitle = pdfTitle;
+    }
+
+    public String getPdfUrl() {
+        return pdfUrl;
+    }
+
+    public void setPdfUrl(String pdfUrl) {
+        this.pdfUrl = pdfUrl;
+    }
+}
--- app/src/main/java/com/trodev/mycoaching/ExamSyllabusActivity.java
+++ app/src/main/java/com/trodev/mycoaching/ExamSyllabusActivity.java
@@ -1,10 +1,37 @@
 package com.trodev.mycoaching;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExamSyllabusActivity extends AppCompatActivity {
+
+    /*import widget*/
+    FloatingActionButton fab;
+
+    private RecyclerView ebookRecycler;
+    private DatabaseReference reference, referenceSeven;
+    private List<EbookData> list;
+    private EbookAdapter adapter;
+
+    ProgressBar progreeBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +41,82 @@ public class ExamSyllabusActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Exam Syllabus");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        /*find this widget with id*/
+        fab = findViewById(R.id.fab);
+        progreeBar = findViewById(R.id.progreeBar);
+
+        /*set on click listener*/
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExamSyllabusActivity.this, UploadPDFActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /*pdf data*/
+        ebookRecycler = findViewById(R.id.ebookRecycler);
+        reference = FirebaseDatabase.getInstance().getReference().child("syllabus_class_six");
+        referenceSeven = FirebaseDatabase.getInstance().getReference().child("syllabus_class_seven");
+
+        getData();
+        getDataSeven();
     }
+
+    private void getDataSeven() {
+
+        referenceSeven.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                progreeBar.setVisibility(View.VISIBLE);
+                list = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    EbookData data = snapshot.getValue(EbookData.class);
+                    list.add(data);
+                }
+
+                progreeBar.setVisibility(View.INVISIBLE);
+                adapter = new EbookAdapter(ExamSyllabusActivity.this, list);
+                // progressBar.setVisibility(View.GONE);
+                ebookRecycler.setLayoutManager(new LinearLayoutManager(ExamSyllabusActivity.this));
+                ebookRecycler.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                progreeBar.setVisibility(View.GONE);
+                Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void getData() {
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                list = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    EbookData data = snapshot.getValue(EbookData.class);
+                    list.add(data);
+                }
+
+                adapter = new EbookAdapter(ExamSyllabusActivity.this, list);
+                // progressBar.setVisibility(View.GONE);
+                ebookRecycler.setLayoutManager(new LinearLayoutManager(ExamSyllabusActivity.this));
+                ebookRecycler.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //progressBar.setVisibility(View.GONE);
+                Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 }
