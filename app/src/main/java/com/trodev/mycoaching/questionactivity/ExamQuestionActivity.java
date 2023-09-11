f665be1d10df6b49f2a970221d11b0b0e8d7a8f8
zobayerdev <zobayer.dev@gmail.com>
2023-09-12 03:01:35 +0600

    exam question is six to eight complete

--- app/src/main/java/com/trodev/mycoaching/questionactivity/ExamQuestionActivity.java
+++ app/src/main/java/com/trodev/mycoaching/questionactivity/ExamQuestionActivity.java
@@ -26,11 +26,11 @@ public class ExamQuestionActivity extends AppCompatActivity {
 
     /*import widget*/
     FloatingActionButton fab;
-    private RecyclerView questionRv, qustionsevenRv ;
-    private DatabaseReference reference, referenceSeven;
+    private RecyclerView questionRv, qustionsevenRv, qustioneightRv ;
+    private DatabaseReference reference, referenceSeven, referenceEight, referenceNine, referenceTen, referenceHsc;
     private List<QuestionData> list;
     private QuestionAdapter adapter;
-    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +42,7 @@ public class ExamQuestionActivity extends AppCompatActivity {
 
         /*find this widget with id*/
         fab = findViewById(R.id.fab);
-        progressBar = findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.VISIBLE);
+
 
         /*set on click listener*/
         fab.setOnClickListener(new View.OnClickListener() {
@@ -57,16 +56,25 @@ public class ExamQuestionActivity extends AppCompatActivity {
         /*pdf data*/
         questionRv = findViewById(R.id.questionRv);
         qustionsevenRv = findViewById(R.id.qustionsevenRv);
-       // reference = FirebaseDatabase.getInstance().getReference().child("question_class_six");
+        qustioneightRv = findViewById(R.id.qustioneightRv);
+
+
+        /*database path*/
         reference = FirebaseDatabase.getInstance().getReference().child("questions").child("question_class_six");
         referenceSeven = FirebaseDatabase.getInstance().getReference().child("questions").child("question_class_seven");
+        referenceEight = FirebaseDatabase.getInstance().getReference().child("questions").child("question_class_eight");
+        referenceNine = FirebaseDatabase.getInstance().getReference().child("questions").child("question_class_nine");
+        referenceTen = FirebaseDatabase.getInstance().getReference().child("questions").child("question_class_ten");
+        referenceHsc = FirebaseDatabase.getInstance().getReference().child("questions").child("question_class_hsc");
 
         /*offline mode create*/
         reference.keepSynced(true);
         referenceSeven.keepSynced(true);
+        referenceEight.keepSynced(true);
 
         getData();
         getDataSeven();
+        getDataEight();
     }
 
     private void getData() {
@@ -82,20 +90,17 @@ public class ExamQuestionActivity extends AppCompatActivity {
                 }
 
                 adapter = new QuestionAdapter(ExamQuestionActivity.this, list);
-                progressBar.setVisibility(View.GONE);
                 questionRv.setLayoutManager(new LinearLayoutManager(ExamQuestionActivity.this));
                 questionRv.setAdapter(adapter);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                progressBar.setVisibility(View.VISIBLE);
                 Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
-
 
     private void getDataSeven() {
 
@@ -103,14 +108,13 @@ public class ExamQuestionActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                progressBar.setVisibility(View.VISIBLE);
                 list = new ArrayList<>();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     QuestionData data = snapshot.getValue(QuestionData.class);
                     list.add(data);
                 }
 
-                progressBar.setVisibility(View.INVISIBLE);
+
                 adapter = new QuestionAdapter(ExamQuestionActivity.this, list);
                 // progressBar.setVisibility(View.GONE);
                 qustionsevenRv.setLayoutManager(new LinearLayoutManager(ExamQuestionActivity.this));
@@ -119,7 +123,32 @@ public class ExamQuestionActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                progressBar.setVisibility(View.GONE);
+                Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void getDataEight() {
+
+        referenceEight.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                list = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    QuestionData data = snapshot.getValue(QuestionData.class);
+                    list.add(data);
+                }
+
+
+                adapter = new QuestionAdapter(ExamQuestionActivity.this, list);
+                qustioneightRv.setLayoutManager(new LinearLayoutManager(ExamQuestionActivity.this));
+                qustioneightRv.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
             }
         });
